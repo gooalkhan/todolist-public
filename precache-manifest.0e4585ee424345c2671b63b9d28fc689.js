b9791dcfee0ea0f3e484d25d86540fa8f9734b6a
gooalkhan <gooalkhan@gmail.com>
2020-08-22 00:36:50 +0900

    Updates

---
+++ precache-manifest.0e4585ee424345c2671b63b9d28fc689.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca73e325bb966d4e94b3c533e9ff2946",
+    "url": "/todolist-public/index.html"
+  },
+  {
+    "revision": "fbc1f7b1c97dac0107aa",
+    "url": "/todolist-public/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "40d5ef60ca996a29c848",
+    "url": "/todolist-public/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fbc1f7b1c97dac0107aa",
+    "url": "/todolist-public/static/js/2.6e06adc1.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/todolist-public/static/js/2.6e06adc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40d5ef60ca996a29c848",
+    "url": "/todolist-public/static/js/main.1995f68f.chunk.js"
+  },
+  {
+    "revision": "b9a49b59ab11c5d26e47",
+    "url": "/todolist-public/static/js/runtime-main.6f51bab0.js"
+  }
+]);
